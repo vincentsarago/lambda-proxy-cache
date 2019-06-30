985e6781a0bfec7ea63748fe9ae8fb6f9834a63e
vincentsarago <vincent.sarago@gmail.com>
2019-06-29 22:52:44 -0400

    dev

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ extra_reqs = {
 
 setup(
     name="lambda-proxy-cache",
-    version="0.0.1",
+    version="0.0.1dev0",
     description=u"Add cache to lambda-proxy",
     long_description=readme,
     long_description_content_type="text/markdown",
